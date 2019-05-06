5759b35af875b26205229d3c249615dd7f31a7dc
ezequiel rios <riosezequielangel@gmail.com>
2019-05-06 18:06:39 -0300

    reloj

---
+++ Unidad2Ejercicios/Unidad2Ejercicios/Reloj1_16.vb
@@ -0,0 +1,8 @@
+﻿Imports System
+Module Reloj1_16
+    Sub Main()
+        Dim Fecha As Date =
+        Console.WriteLine("Dia y Año actual: ")
+
+    End Sub
+End Module
---
+++ unidad02Cadenas/unidad02Cadenas/Refrase_1_15.vb
@@ -0,0 +1,10 @@
+﻿Module Refrase_1_15
+    Sub Main()
+        Dim fecha As Date = Now
+
+        Console.WriteLine("Es el dia {0} del año {1} ", fecha.Day, fecha.Year)
+        Console.WriteLine("Mes {0} , hora y minuto {1} ", fecha.Month, fecha.TimeOfDay)
+        Console.ReadKey()
+
+    End Sub
+End Module
