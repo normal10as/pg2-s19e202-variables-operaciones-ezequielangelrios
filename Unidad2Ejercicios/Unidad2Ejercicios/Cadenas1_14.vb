ab1996952ce3a9ce41e274d8cc7d64f4436b489a
ezequiel rios <riosezequielangel@gmail.com>
2019-05-06 16:36:58 -0300

    cadenas framework

---
+++ Unidad2Ejercicios/Unidad2Ejercicios/Cadenas1_14.vb
@@ -0,0 +1,15 @@
+﻿Imports System.Runtime.CompilerServices
+
+Module Cadenas1_14
+    Sub Main()
+        Dim Palabra As String
+        Dim BuscarChar As String = "a"
+        Dim BuscarPos As Byte = 1
+        Console.WriteLine("Ingrese una palabra: ")
+        Palabra = Console.ReadLine
+        Console.WriteLine("Buscar {0} en {1}, en la posision {2}", BuscarChar, Palabra, BuscarPos)
+        Console.WriteLine(Instr(BuscarPos, Palabra, BuscarChar, CompareMethod.text))
+
+
+    End Sub
+End Module
---
+++ unidad02Cadenas/unidad02Cadenas/Cadenas1_14.vb
@@ -0,0 +1,25 @@
+﻿Module Cadenas1_14
+
+    Sub Main()
+        Dim Palabra As String
+        Dim BuscarCharA As String = "a"
+        Dim BuscarCharO As String = "o"
+        Dim BuscarPos As Byte = 1
+        Console.WriteLine("Ingrese una palabra: ")
+        Palabra = Console.ReadLine
+        'Busqueda de A
+        Console.WriteLine("Buscar {0} en {1}, en la posicion {2}", BuscarCharA, Palabra, BuscarPos)
+        Console.WriteLine("La letra a aparece en la {0} posicion",
+                          InStr(BuscarPos, Palabra, BuscarCharA, CompareMethod.Text))
+        'Busqueda de O
+        Console.WriteLine("Buscar {0} en {1}, en la posicion {2}", BuscarCharO, Palabra, BuscarPos)
+        Console.WriteLine("La letra a aparece en la {0} posicion",
+                          InStr(BuscarPos, Palabra, BuscarCharO, CompareMethod.Text))
+
+
+
+        Console.ReadKey()
+
+    End Sub
+
+End Module
