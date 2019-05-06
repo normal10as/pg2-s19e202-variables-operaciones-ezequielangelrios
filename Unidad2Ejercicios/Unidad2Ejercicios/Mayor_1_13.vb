82fb9f868cbe5204b72b1846385ecf4f1ca8890a
ezequiel rios <riosezequielangel@gmail.com>
2019-05-06 15:07:41 -0300

    mayor

---
+++ Unidad2Ejercicios/Unidad2Ejercicios/Mayor_1_13.vb
@@ -0,0 +1,16 @@
+﻿Imports System.Math
+Module Mayor_1_13
+    Sub Main()
+        Dim Valor1 As Byte
+        Dim Valor2 As Byte
+        Dim Valor3 As Byte
+        Dim Mayor12 As Byte
+        Console.WriteLine("Ingrese tres valores: ")
+        Valor1 = Console.ReadLine
+        Valor2 = Console.ReadLine
+        Valor3 = Console.ReadLine
+        Mayor12 = Max(Valor1, Valor2)
+        Console.WriteLine("Mayor: {0} ", Max(Mayor12, Valor3))
+
+    End Sub
+End Module
