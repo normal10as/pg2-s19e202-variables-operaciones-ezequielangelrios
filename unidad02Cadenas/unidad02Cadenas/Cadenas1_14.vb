39ed1040e01fa52e3f1db319414523d87c4bf322
ezequiel rios <riosezequielangel@gmail.com>
2019-05-06 16:52:51 -0300

    palabra invertida

--- unidad02Cadenas/unidad02Cadenas/Cadenas1_14.vb
+++ unidad02Cadenas/unidad02Cadenas/Cadenas1_14.vb
@@ -2,21 +2,21 @@
 
     Sub Main()
         Dim Palabra As String
-        Dim BuscarCharA As String = "a"
-        Dim BuscarCharO As String = "o"
-        Dim BuscarPos As Byte = 1
+        Dim BuscarCharA As String = "A"
+        Dim BuscarCharO As String = "O"
+
         Console.WriteLine("Ingrese una palabra: ")
         Palabra = Console.ReadLine
         'Busqueda de A
-        Console.WriteLine("Buscar {0} en {1}, en la posicion {2}", BuscarCharA, Palabra, BuscarPos)
-        Console.WriteLine("La letra a aparece en la {0} posicion",
-                          InStr(BuscarPos, Palabra, BuscarCharA, CompareMethod.Text))
+        Console.WriteLine("Buscar {0} en {1}, en la posicion {2}", BuscarCharA, Palabra, 1)
+        Console.WriteLine("La letra A aparece en la {0} posicion",
+                          InStr(1, Palabra, BuscarCharA, CompareMethod.Text))
         'Busqueda de O
-        Console.WriteLine("Buscar {0} en {1}, en la posicion {2}", BuscarCharO, Palabra, BuscarPos)
-        Console.WriteLine("La letra a aparece en la {0} posicion",
-                          InStr(BuscarPos, Palabra, BuscarCharO, CompareMethod.Text))
+        Console.WriteLine("Buscar {0} en {1}, en la posicion {2}", BuscarCharO, Palabra, 1)
+        Console.WriteLine("La letra O aparece en la {0} posicion",
+                          InStr(1, Palabra, BuscarCharO, CompareMethod.Text))
 
-
+        Console.WriteLine("Palabra Invertida: " & StrReverse(Palabra))
 
         Console.ReadKey()
 
