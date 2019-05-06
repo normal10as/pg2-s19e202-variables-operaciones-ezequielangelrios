e582e4447bf51cd65a63f81cf681cf560cae4b2b
ezequiel rios <riosezequielangel@gmail.com>
2019-05-06 01:13:13 -0300

    resto

---
+++ Unidad2Ejercicios/Unidad2Ejercicios/Resto_Division.vb
@@ -0,0 +1,9 @@
+﻿Module Resto_Division
+    Sub Main()
+        Dim Valor1 As UShort
+        Console.WriteLine("Ingrese un valor: ")
+        Valor1 = Console.ReadLine
+        Console.WriteLine("Resto : {0} ", Valor1 / 2)
+
+    End Sub
+End Module
