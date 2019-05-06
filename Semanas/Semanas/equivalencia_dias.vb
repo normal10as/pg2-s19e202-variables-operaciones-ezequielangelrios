92f158fcafa73f0959a1d67b326c3235cf165b15
ezequiel rios <riosezequielangel@gmail.com>
2019-05-05 21:39:44 -0300

    equivalencia dias

---
+++ Semanas/Semanas/equivalencia_dias.vb
@@ -0,0 +1,25 @@
+﻿Imports System
+Module equivalencia_dias
+    Const DiasPorSemana As Byte = 7
+    Const HorasPorDia As Byte = 24
+    Const MinutosPorHora As Byte = 60
+    Sub Main()
+        Dim IngresoDias As UShort
+        Dim NumeroDeSemanas As UShort
+        Dim NumeroHoras As UShort
+        Dim NumeroMinutos As UInteger
+
+        Console.WriteLine("Ingrese numero de dias: ")
+        IngresoDias = Console.ReadLine
+        NumeroDeSemanas = IngresoDias / DiasPorSemana
+        NumeroHoras = IngresoDias * HorasPorDia
+        NumeroMinutos = IngresoDias * HorasPorDia * MinutosPorHora
+        Console.WriteLine("Numero de dias {0}, total de semanas {1}, total de horas {2}, total de minutos {3}
+        ", IngresoDias, NumeroDeSemanas, NumeroHoras, NumeroMinutos)
+
+
+
+
+
+    End Sub
+End Module
