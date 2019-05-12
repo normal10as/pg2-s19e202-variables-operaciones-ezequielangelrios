fdbd7c36671522879a96a8631bd21fea2307ac65
ezequiel rios <riosezequielangel@gmail.com>
2019-05-12 17:22:25 -0300

    date

--- Unidad2Ejercicios/Unidad2Ejercicios/Reloj1_16.vb
+++ Unidad2Ejercicios/Unidad2Ejercicios/Reloj1_16.vb
@@ -1,8 +1,7 @@
-﻿Imports System
+﻿Imports System.DateTime
 Module Reloj1_16
     Sub Main()
-        Dim fecha As Date
-
+        Dim fecha As Date = Now
         Console.WriteLine("Es el dia {0} del año {1} ", fecha.Day, fecha.Year)
         Console.WriteLine("Mes {0} , hora y minuto {1} ", fecha.Month, fecha.TimeOfDay)
         Console.ReadKey()
