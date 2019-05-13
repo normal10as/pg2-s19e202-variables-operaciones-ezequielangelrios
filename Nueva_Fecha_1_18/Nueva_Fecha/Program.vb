0c789cf382322e5c5d791c7cadc99f6a941164a7
ezequiel rios <riosezequielangel@gmail.com>
2019-05-13 13:48:57 -0300

    nueva fecha

--- Nueva_Fecha_1_18/Nueva_Fecha/Program.vb
+++ Nueva_Fecha_1_18/Nueva_Fecha/Program.vb
@@ -3,15 +3,15 @@ Imports System.DateTime
 Module Program
     Sub Main(args As String())
         Dim Fecha As Date
-        Dim AddFecha As Byte
+        Dim NuevaFecha As Byte
 
 
         Console.WriteLine("Ingrese una fecha (10/10/2019: ")
         Fecha = Console.ReadLine
         Console.WriteLine("Ingrese los dias a sumar: ")
-        AddFecha = Console.ReadLine
+        NuevaFecha = Console.ReadLine
 
-        Console.WriteLine("La suma de las fechas ingresadas es: " & Fecha.AddDays(AddFecha))
+        Console.WriteLine("La suma de las fechas ingresadas es: " & Fecha.AddDays(NuevaFecha))
 
     End Sub
 End Module
